eaaf0e1ef676e4e6d9f8b90303f51d8da322b454
Jeff Flynn <96162611+mula-jeff@users.noreply.github.com>
2024-08-19 10:41:59 +0700

    Initial Commit

---
+++ Evolution/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Evolution
+//
+//  Created by Jeffrey Michael Flynn on 19/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Evolution/EvolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvolutionApp.swift
+//  Evolution
+//
+//  Created by Jeffrey Michael Flynn on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EvolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

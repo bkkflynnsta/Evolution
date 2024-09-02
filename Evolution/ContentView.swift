e8a49f82924f8f8f90689193432728f9049e349d
Jeff Flynn <96162611+mula-jeff@users.noreply.github.com>
2024-09-02 13:48:28 +0700

    updated formatting for better readability

--- Evolution/ContentView.swift
+++ Evolution/ContentView.swift
@@ -17,14 +17,14 @@ import SwiftUI
 // First View to display the root of the tree
 struct PhyloTreeView: View {
     var rootNode: PhyloNode
-
+    
     var body: some View {
         NavigationView {
             VStack {
                 Text("Phylogenetic Tree")
                     .font(.largeTitle)
                     .padding()
-
+                
                 List(rootNode.children) { childNode in
                     NavigationLink(destination: PhyloNodeDetailView(node: childNode)) {
                         Text(childNode.name)
@@ -39,13 +39,13 @@ struct PhyloTreeView: View {
 // Second View to display details and navigate deeper into the tree
 struct PhyloNodeDetailView: View {
     var node: PhyloNode
-
+    
     var body: some View {
         VStack {
             Text("\(node.level): \(node.name)")
                 .font(.title)
                 .padding()
-
+            
             if !node.children.isEmpty {
                 List(node.children) { childNode in
                     NavigationLink(destination: PhyloNodeDetailView(node: childNode)) {
